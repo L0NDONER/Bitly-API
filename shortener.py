8b40a1876b2a9f1c488a4fd2fd9b4e39748c67b3
L0NDONER <oflaherty@gmail.com>
2021-01-08 23:11:10 +0000

    Update shortener.py

--- shortener.py
+++ shortener.py
@@ -3,7 +3,7 @@
 import bitly_api
 import sys
 
-#Define Bitly Api Details
+# Define Bitly Api Details
 
 API_USER = "*****"  # USERNAME
 API_KEY = "****************"  # API KEY
@@ -13,10 +13,10 @@ usage = """ Usage: python shortener.py [url]
 e.g python shortener.py http://www.google.com """
 
 if len(sys.argv) != 2:
-	print usage
-	sys.exit(0)
+    print usage
+    sys.exit(0)
 
-longurl	= sys.argv[1]
+longurl = sys.argv[1]
 
 response = b.shorten(uri=longurl)
 
